2f17981fc0c3f96221c318723e29ce6d515ceb8d
Dan Jones <dan.jones@moneydesktop.com>
2017-01-03 11:05:52 -0700

    Add Account#transactions spec

--- spec/lib/atrium/account_spec.rb
+++ spec/lib/atrium/account_spec.rb
@@ -125,4 +125,86 @@ describe ::Atrium::Account do
       expect(response.user_guid).to eq(account_attributes[:user_guid])
     end
   end
+
+  describe "transactions" do
+    let(:account) { ::Atrium::Account.new(account_attributes) }
+    let(:account_transactions_response) { ::JSON.parse(raw_account_transactions_response) }
+    let(:transaction_attributes) do
+      {
+        :account_guid => account_attributes[:guid],
+        :amount => 61.11,
+        :category => "Groceries",
+        :check_number => nil,
+        :created_at => "2016-10-06T09:43:42+00:00",
+        :date => "2013-09-23",
+        :description => "Whole Foods",
+        :guid => "TRN-265abee9-889b-af6a-c69b-25157db2bdd9",
+        :is_bill_pay => false,
+        :is_direct_deposit => false,
+        :is_expense => true,
+        :is_fee => false,
+        :is_income => false,
+        :is_overdraft_fee => false,
+        :is_payroll_advance => false,
+        :latitude => -43.2075,
+        :longitude => 139.691706,
+        :member_guid => "MBR-7c6f361b-e582-15b6-60c0-358f12466b4b",
+        :memo => nil,
+        :merchant_category_code => 5411,
+        :original_description => "WHOLEFDS TSQ 102",
+        :posted_at => "2016-10-07T06:00:00+00:00",
+        :status => "POSTED",
+        :top_level_category => "Food & Dining",
+        :transacted_at => "2016-10-06T13:00:00+00:00",
+        :type => "DEBIT",
+        :updated_at => "2016-10-07T05:49:12+00:00",
+        :user_guid => "USR-fa7537f3-48aa-a683-a02a-b18940482f54"
+      }
+    end
+
+    let(:raw_account_transactions_response) {
+      { :transactions => [transaction_attributes, transaction_attributes] }.to_json
+    }
+
+    before {
+      allow(::Atrium.client).to receive(:make_request).and_return(account_transactions_response)
+    }
+
+    it "returns list of transactions for account" do
+      response = account.transactions
+
+      expect(response).to be_kind_of(::Array)
+      expect(response.length).to eq(2)
+      expect(response.first).to be_kind_of(::Atrium::Transaction)
+
+      expect(response.first.account_guid).to eq(transaction_attributes[:account_guid])
+      expect(response.first.amount).to eq(transaction_attributes[:amount])
+      expect(response.first.category).to eq(transaction_attributes[:category])
+      expect(response.first.check_number).to eq(transaction_attributes[:check_number])
+      expect(response.first.created_at).to eq(transaction_attributes[:created_at])
+      expect(response.first.date).to eq(transaction_attributes[:date])
+      expect(response.first.description).to eq(transaction_attributes[:description])
+      expect(response.first.guid).to eq(transaction_attributes[:guid])
+      expect(response.first.is_bill_pay).to eq(transaction_attributes[:is_bill_pay])
+      expect(response.first.is_direct_deposit).to eq(transaction_attributes[:is_direct_deposit])
+      expect(response.first.is_expense).to eq(transaction_attributes[:is_expense])
+      expect(response.first.is_fee).to eq(transaction_attributes[:is_fee])
+      expect(response.first.is_income).to eq(transaction_attributes[:is_income])
+      expect(response.first.is_overdraft_fee).to eq(transaction_attributes[:is_overdraft_fee])
+      expect(response.first.is_payroll_advance).to eq(transaction_attributes[:is_payroll_advance])
+      expect(response.first.latitude).to eq(transaction_attributes[:latitude])
+      expect(response.first.longitude).to eq(transaction_attributes[:longitude])
+      expect(response.first.member_guid).to eq(transaction_attributes[:member_guid])
+      expect(response.first.memo).to eq(transaction_attributes[:memo])
+      expect(response.first.merchant_category_code).to eq(transaction_attributes[:merchant_category_code])
+      expect(response.first.original_description).to eq(transaction_attributes[:original_description])
+      expect(response.first.posted_at).to eq(transaction_attributes[:posted_at])
+      expect(response.first.status).to eq(transaction_attributes[:status])
+      expect(response.first.top_level_category).to eq(transaction_attributes[:top_level_category])
+      expect(response.first.transacted_at).to eq(transaction_attributes[:transacted_at])
+      expect(response.first.type).to eq(transaction_attributes[:type])
+      expect(response.first.updated_at).to eq(transaction_attributes[:updated_at])
+      expect(response.first.user_guid).to eq(transaction_attributes[:user_guid])
+    end
+  end
 end

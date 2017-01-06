065af69c258ede09cde30dfcdf86abd263e3bf1d
Dan Jones <dan.jones@moneydesktop.com>
2017-01-06 14:56:49 -0700

    Add institution credentials spec

--- spec/lib/atrium/institution_spec.rb
+++ spec/lib/atrium/institution_spec.rb
@@ -8,6 +8,7 @@ describe ::Atrium::Institution do
     ]
   }
   let(:institution) { ::Atrium::Member.new(institution_attributes) }
+  let(:institution_code) { institution_attributes["code"] }
   let(:institution_response) { ::JSON.parse(raw_institution_response)}
   let(:institutions_response) { ::JSON.parse(raw_institutions_response)}
   let(:institution_attributes) do
@@ -38,6 +39,36 @@ describe ::Atrium::Institution do
       expect(response.first.code).to eq(institution_attributes[:code])
       expect(response.first.name).to eq(institution_attributes[:name])
       expect(response.first.url).to eq(institution_attributes[:url])
+    end
+  end
+
+  describe ".credentials" do
+    let(:credential_response) { ::JSON.parse(raw_credential_response)}
+    let(:credentials_response) { ::JSON.parse(raw_credentials_response)}
+    let(:credential_attributes) do
+      {
+        :guid => "CRD-123",
+        :value => "user-name"
+      }
+    end
+    let(:raw_credential_response) {
+      { :credential => credential_attributes }.to_json
+    }
+    let(:raw_credentials_response) {
+      { :credentials => [credential_attributes, credential_attributes]}.to_json
+    }
+
+    before { allow(::Atrium.client).to receive(:make_request).and_return(credentials_response) }
+
+    it "should return credentials for institution" do
+      response = ::Atrium::Institution.credentials(institution_code)
+
+      expect(response).to be_kind_of(::Array)
+      expect(response.length).to eq(2)
+      expect(response.first).to be_kind_of(::Atrium::Credential)
+
+      expect(response.first.guid).to eq(credential_attributes[:guid])
+      expect(response.first.value).to eq(credential_attributes[:value])
     end
   end
 end

123f154d8630f5c4fc78b76fa79112c919254b1b
Dan Jones <dan.jones@moneydesktop.com>
2017-01-11 11:19:01 -0700

    Add Paginate module to Atrium

---
+++ lib/atrium/paginate.rb
@@ -0,0 +1,52 @@
+require "URI"
+
+module Atrium
+  module Paginate
+    DEFAULT_PAGE = 1
+    DEFAULT_RECORDS_PER_PAGE = 25
+    DEFAULT_TOTAL_PAGES = 1
+
+    attr_accessor :current_page, :klass_name, :per_page, :total_pages
+
+    def endpoint_name # (klass)
+      @klass_name ||= self.name.gsub("Atrium::", "").downcase.pluralize
+    end
+
+    def paginate_endpoint(options = nil)
+      endpoint_name
+      set_pagination_fields_from_initial_request(endpoint_name, options)
+      list = []
+
+      until current_page > total_pages
+        endpoint = "#{klass_name}?page=#{current_page}&records_per_page=#{per_page}"
+        response = ::Atrium.client.make_request(:get, endpoint)
+
+        # Add new objects to the list
+        response["#{klass_name}"].map do |params|
+          list << self.new(params)
+        end
+        @current_page += 1
+      end
+      list
+    end
+
+    def response_list(response)
+      response["#{klass_name}"].map do |params|
+        self.new(params)
+      end
+    end
+
+    def set_pagination_fields(response)
+      pagination = response["pagination"]
+      @current_page  = pagination["current_page"] || DEFAULT_PAGE
+      @per_page  = pagination["per_page"] || DEFAULT_RECORDS_PER_PAGE
+      @total_pages  = pagination["total_pages"] || DEFAULT_TOTAL_PAGES
+    end
+
+    # TODO: add options for this!
+    def set_pagination_fields_from_initial_request(endpoint, options = nil)
+      response = ::Atrium.client.make_request(:get, endpoint)
+      set_pagination_fields(response)
+    end
+  end
+end

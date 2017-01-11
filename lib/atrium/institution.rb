7860627148a10fae1cdafd5b6d1da48cad9f29ad
Dan Jones <dan.jones@moneydesktop.com>
2017-01-11 11:19:59 -0700

    Add Pagination to Atrium Institution.rb

--- lib/atrium/institution.rb
+++ lib/atrium/institution.rb
@@ -1,7 +1,6 @@
-require "URI"
-
 module Atrium
   class Institution
+    extend ::Atrium::Paginate
     include ::ActiveAttr::Model
 
     attribute :code
@@ -17,21 +16,16 @@ module Atrium
       end
     end
 
-    def self.list(options = nil)
-      endpoint = format_endpoint("institutions", options)
-      response = ::Atrium.client.make_request(:get, endpoint)
-
-      response["institutions"].map do |institution|
-        ::Atrium::Institution.new(institution)
-      end
-    end
-
     def self.format_endpoint(endpoint, options = nil)
       if options.present?
         endpoint + "?" + URI.encode_www_form(options)
       else
         endpoint
       end
+    end
+
+    def self.list(options = nil)
+      paginate_endpoint(options)
     end
   end
 end

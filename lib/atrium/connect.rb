ba7482ad19305e4209c19854b4ecf1f796fd93f3
Garrett Thornburg <film42@gmail.com>
2018-03-07 15:29:33 -0500

    Add and protect connet widget create options

--- lib/atrium/connect.rb
+++ lib/atrium/connect.rb
@@ -1,7 +1,15 @@
+require "set"
+
 module Atrium
   class Connect
     include ::ActiveAttr::Model
 
+    PERMITTED_CONNECT_CREATE_OPTIONS = ::Set.new([
+      "current_institution_code",
+      "current_member_guid",
+      "is_mobile_webview",
+      "update_credentials",
+    ]).freeze
     SCRIPT_SOURCE = "https://atrium.mx.com/connect.js".freeze
 
     # ATTRIBUTES
@@ -11,9 +19,13 @@ module Atrium
     ##
     # CLASS METHODS
     #
-    def self.create(user_guid:)
+    def self.create(user_guid:, options: {})
+      options.each do |key, _value|
+        fail ArgumentError, "An invalid option was provided: #{key}" if !PERMITTED_CONNECT_CREATE_OPTIONS.include?(key.to_s)
+      end
+
       endpoint = "/users/#{user_guid}/connect_widget_url"
-      connect_response = ::Atrium.client.make_request(:post, endpoint)
+      connect_response = ::Atrium.client.make_request(:post, endpoint, options)
 
       connect_params = connect_response["user"]
       ::Atrium::Connect.new(connect_params)

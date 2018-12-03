235e37cc1cdf3c09173ff9004c23f1ae66873707
brettmortensen <11483419+brettmortensen@users.noreply.github.com>
2018-12-03 16:43:05 -0700

    Remove additional branding

--- lib/atrium-ruby/api_client.rb
+++ lib/atrium-ruby/api_client.rb
@@ -27,7 +27,7 @@ module MX
     # @option config [Configuration] Configuration for initializing the object, default to Configuration.default
     def initialize(config = Configuration.default)
       @config = config
-      @user_agent = "Swagger-Codegen/#{VERSION}/ruby"
+      @user_agent = "MX-Codegen/#{VERSION}/ruby"
       @default_headers = {
         'Content-Type' => 'application/json',
         'User-Agent' => @user_agent
